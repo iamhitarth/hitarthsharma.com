b5d4c900cdb34d189d85ce99d083be50ceca5fb9
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-07-10 18:11:27 +1000

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Hitarth Sharma',
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    'gatsby-plugin-react-helmet',
+  ],
+}
