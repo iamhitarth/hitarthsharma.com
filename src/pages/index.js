0e231096085578e274457ab85f587b659a2c143b
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-07-11 23:52:23 +1000

    Added file system source plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to my new Gatsby site.</p>
-    <p>Now lets build something great.</p>
+    <div>
+      <h1>My Site's Files</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>relativePath</th>
+            <th>prettySize</th>
+            <th>extension</th>
+            <th>birthTime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allFile.edges.map(({ node }, index) => (
+            <tr key={index}>
+              <td>{node.relativePath}</td>
+              <td>{node.prettySize}</td>
+              <td>{node.extension}</td>
+              <td>{node.birthTime}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
     <Link to="/about/">Go to About me</Link>
   </div>
 )
+
+export const query = graphql`
+  query MyFilesQuery {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
