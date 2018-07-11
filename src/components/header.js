a2b7b8208e2e87bbac7639d82a61edbde527d588
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-07-11 13:54:04 +1000

    Added top nav

--- src/components/header.js
+++ src/components/header.js
@@ -13,21 +13,44 @@ const Header = ({ siteTitle }) => (
         margin: '0 auto',
         maxWidth: 960,
         padding: '1.45rem 1.0875rem',
+        display: 'flex',
+        justifyContent: 'space-between',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+      <h1 style={styles.heading}>
+        <Link to="/" style={styles.headerLink}>
           {siteTitle}
         </Link>
       </h1>
+      <ul style={styles.nav}>
+        <li style={styles.navItem}>
+          <Link to="/" style={styles.headerLink}>
+            Home
+          </Link>
+        </li>
+        <li style={styles.navItem}>
+          <Link to="/about" style={styles.headerLink}>
+            About
+          </Link>
+        </li>
+      </ul>
     </div>
   </div>
 )
 
 export default Header
+
+const styles = {
+  heading: { margin: 0 },
+  headerLink: {
+    color: 'white',
+    textDecoration: 'none',
+  },
+  nav: {
+    margin: 0,
+    listStyleType: 'none',
+    display: 'flex',
+    alignItems: 'flex-end',
+  },
+  navItem: { display: 'inline ', margin: 0, paddingLeft: 20 },
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>Now lets build something great.</p>
+    <Link to="/about/">Go to About me</Link>
   </div>
 )
 
