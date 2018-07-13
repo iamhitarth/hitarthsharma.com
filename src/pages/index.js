ef3413779fe2b7a49f4da11fbfc9554eab03998a
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-07-13 23:08:59 +1000

    Added time to read on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,46 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = ({ data }) => (
-  <div>
-    {data.allMarkdownRemark.edges.map(({ node }) => (
-      <div key={node.id}>
-        <Link
-          to={node.fields.slug}
-          style={{
-            textDecoration: `none`,
-            color: `inherit`,
-            fontWeight: `normal`,
-          }}
-        >
-          <h3 style={{ marginBottom: '1em' }}>
-            {node.frontmatter.title}{' '}
-            <span style={{ color: '#BBB' }}>— {node.fields.postDate}</span>
-          </h3>
-          <p>{node.excerpt}</p>
-        </Link>
-      </div>
-    ))}
-  </div>
-)
+const IndexPage = ({ data }) => {
+  return (
+    <div>
+      {data.allMarkdownRemark.edges.map(({ node }) => {
+        console.log('***Node', node)
+        return (
+          <div key={node.id}>
+            <Link
+              to={node.fields.slug}
+              style={{
+                textDecoration: `none`,
+                color: `inherit`,
+                fontWeight: `normal`,
+              }}
+            >
+              <div
+                style={{
+                  marginBottom: '1em',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <h3 style={{ display: 'flex' }}>
+                  {node.frontmatter.title}{' '}
+                  <span style={{ color: '#BBB' }}>
+                    — {node.fields.postDate}
+                  </span>
+                </h3>
+                <h3 style={{ color: '#BBB' }}>
+                  {node.timeToRead || 1} {node.timeToRead > 1 ? 'mins' : 'min'}.
+                </h3>
+              </div>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
 
 export const query = graphql`
   query IndexQuery {
@@ -35,6 +53,8 @@ export const query = graphql`
             title
           }
           excerpt
+          html
+          timeToRead
           fields {
             slug
             postDate(formatString: "DD MMMM, YYYY")
--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -14,6 +14,7 @@ export const query = graphql`
   query BlogPostQuery($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      timeToRead
       frontmatter {
         title
       }
