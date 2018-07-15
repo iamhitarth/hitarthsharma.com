76098a9910ebb84f7a8a5c1cdfd93f792f2cabd9
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-07-15 16:21:10 +1000

    Added avatar and site title to sidebar layout

--- src/components/sidebarLayout.js
+++ src/components/sidebarLayout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, graphql, Link } from 'gatsby'
 
 /*
 This layout will have no header
@@ -44,10 +44,41 @@ const SidebarLayout = ({ children, location }) => {
               margin: '0 auto',
               maxWidth: 960,
               padding: '0px 1.0875rem 1.45rem',
-              paddingTop: 0,
+              paddingTop: '3rem',
+              display: 'flex',
             }}
           >
-            {children}
+            <div
+              style={{
+                flex: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <div>
+                <img
+                  src="http://www.thecodetoawesome.com/wp-content/uploads/2015/04/cropped-208191_4324406504567_1758127073_n1.jpg"
+                  style={{
+                    height: '11.25rem',
+                    width: '11.25rem',
+                    borderRadius: '50%',
+                  }}
+                  alt="Hitarth Sharma"
+                />
+              </div>
+              <div>
+                <h1>
+                  <Link
+                    to="/"
+                    style={{ textDecoration: 'none', color: 'hsla(0,0%,0%,1)' }}
+                  >
+                    {data.site.siteMetadata.title}
+                  </Link>
+                </h1>
+              </div>
+            </div>
+            <div style={{ flex: 2.5, paddingLeft: '2rem' }}>{children}</div>
           </div>
         </div>
       )}
