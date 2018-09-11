21898c901f30025c3229bd7b9fa0905ab53dce4b
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-09-12 09:38:04 +1000

    Added TODOs

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import SidebarLayout from '../components/sidebarLayout'
 import PostListItem from '../components/postListItem'
 
 /* TODO:
+[] Add cover image for post list item on the home page
+[] Experiment with turniing on showing whole post vs showing excerpt of post
+[] Mobile view where sidebar disappears
 [] Decide on a typography theme and remove the others
 */
 
