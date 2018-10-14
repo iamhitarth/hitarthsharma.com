77c6e407d3244a6910690e72282fc3c2694083b2
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-10-15 08:57:06 +1100

    Setup Jest correctly and wrote tests for Header

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -1,3 +1,22 @@
-test('Adding 1+2 equals 3', () => {
-  expect(1 + 2).toEqual(3)
+import React from 'react'
+import { render } from 'react-testing-library'
+import Header from './header'
+
+describe('Header', () => {
+  const siteTitle = 'Hitarth Sharma'
+  const { getByText, container } = render(<Header siteTitle={siteTitle} />)
+
+  it('matches snapshot', () => {
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('displays site title', () => {
+    const siteTitleElement = getByText(siteTitle)
+    expect(siteTitleElement).toHaveTextContent(siteTitle)
+  })
+
+  it('displays nav links', () => {
+    expect(getByText('Home')).not.toBeNull()
+    expect(getByText('About')).not.toBeNull()
+  })
 })
