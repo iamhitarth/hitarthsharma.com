2901472c28690520365abde7bacf8c59a7014b98
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-10-27 14:08:55 +1100

    Added post title to the page title

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
 
 export const query = graphql`
   query($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       timeToRead
@@ -19,6 +25,20 @@ export default ({ data, location }) => {
   const post = data.markdownRemark
   return (
     <Layout location={location}>
+      <Helmet
+        title={
+          post.frontmatter.title
+            ? `${post.frontmatter.title} - ${data.site.siteMetadata.title}`
+            : `${data.site.siteMetadata.title}`
+        }
+        meta={[
+          { name: 'description', content: "Hitarth Sharma's site" },
+          {
+            name: 'keywords',
+            content: 'hitarth, sharma, blog, software, engineer',
+          },
+        ]}
+      />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
