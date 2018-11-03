57aad3f170efab497c7dd42a81fc32762f492b58
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-11-03 23:58:36 +1100

    Removed todos and tweaked sidebar layout style

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -6,16 +6,6 @@ import styled from 'styled-components'
 import SidebarLayout from '../components/sidebarLayout'
 import PostListItem from '../components/postListItem'
 
-/* TODO: https://www.gatsbyjs.org/docs/adding-tags-and-categories-to-blog-posts/
-- Style tags page - better styling for individual tags and responsive
-- Fix link for tags with a space (need to add a - in the middle)
-- Display tags on the posts page
-Add tags to all posts
-Style tag page - make heading center when on a mobile phone
-Missing favicon.ico
-Switch to book list asap
-*/
-
 const SeeAllTagsWrapper = styled.div`
   padding-bottom: 0.5rem;
 `
