56cd9b430e259c98227b215eb55d400b651049ac
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-11-05 09:32:44 +1100

    Added radio buttons for filtering book list. Need to wire them up.

--- src/pages/book-list.js
+++ src/pages/book-list.js
@@ -10,6 +10,8 @@ Show read and unread links with count of each
 Links to Amazon for each book maybe
  */
 
+const MyBooks = styled.div``
+
 const MyBooksList = styled.ul`
   display: flex;
   flex-direction: column;
@@ -27,10 +29,15 @@ const MyBooksList = styled.ul`
 class BookList extends React.Component {
   state = {
     books: [],
+    read: 0,
+    unread: 0,
+    filterBy: 'all',
   }
 
   componentDidMount() {
     const readingListUrl = 'https://runkit.io/iamhitarth/reading-list-api/4.0.0'
+    let booksRead = 0
+    let booksUnread = 0
 
     get(readingListUrl, this)
       .then(JSON.parse)
@@ -45,9 +52,14 @@ class BookList extends React.Component {
                   ? -1
                   : 1
         )
+        sorted.forEach(
+          book => (book.state === 'complete' ? booksRead++ : booksUnread++)
+        )
 
         this.setState({
           books: [...sorted],
+          read: booksRead,
+          unread: booksUnread,
         })
       })
   }
@@ -56,6 +68,10 @@ class BookList extends React.Component {
     if (this.cancelRequest) {
       this.cancelRequest('Cancelled request on unmount')
     }
+  }
+
+  handleFilterChange(target) {
+    console.log('Filter changed to', target)
   }
 
   renderBooks() {
@@ -69,13 +85,44 @@ class BookList extends React.Component {
   render() {
     const { location } = this.props
     const areBooksLoaded = this.state.books.length > 0
+    const { unread, read, filterBy } = this.state
+    console.log('Checked for all', this.state)
     return (
       <Layout location={location}>
         <div>
           <PostTitle>My Book List</PostTitle>
           <p>What I have read, am reading and would like to read.</p>
           {areBooksLoaded ? (
-            <MyBooksList>{this.renderBooks()}</MyBooksList>
+            <MyBooks>
+              <p>
+                Show{' '}
+                <label className="button">
+                  <input
+                    type="radio"
+                    checked={filterBy === 'all'}
+                    onChange={this.handleFilterChange}
+                  />
+                  {`All (${read + unread})`}
+                </label>{' '}
+                <label className="button">
+                  <input
+                    type="radio"
+                    checked={filterBy === 'read'}
+                    onChange={this.handleFilterChange}
+                  />
+                  {`Read (${read})`}
+                </label>{' '}
+                <label className="button">
+                  <input
+                    type="radio"
+                    checked={filterBy === 'unread'}
+                    onChange={this.handleFilterChange}
+                  />
+                  {`Unread (${unread})`}
+                </label>
+              </p>
+              <MyBooksList>{this.renderBooks()}</MyBooksList>
+            </MyBooks>
           ) : (
             <p>Loading books...</p>
           )}
