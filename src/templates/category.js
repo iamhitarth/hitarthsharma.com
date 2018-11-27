087623d61b17246ee7b8f74c1c11c4cb5369605a
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-11-27 20:35:25 +1100

    Started collecting categories from posts

---
+++ src/templates/category.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import SidebarLayout from '../components/SidebarLayout'
+
+const Tag = ({ pageContext, data, location }) => {
+  return (
+    <SidebarLayout location={location}>
+      This is a category template
+    </SidebarLayout>
+  )
+}
