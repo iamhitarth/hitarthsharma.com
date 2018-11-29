8512d5ccdf38679f46c00b2c858bf640119ae35c
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-11-30 06:38:09 +1100

    Fleshed out category template

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import SidebarLayout from '../components/sidebarLayout'
+import PostListItem from '../components/postListItem'
+
+const SeeAllCategoriesWrapper = styled.div`
+  padding-bottom: 0.5rem;
+`
 
 const Category = ({ pageContext, data, location }) => {
   const { category } = pageContext
   const { edges, totalCount } = data.allMarkdownRemark
-  console.log('Category on the page is ', data)
+  const categoryHeader = `${totalCount} post${
+    totalCount === 1 ? '' : 's'
+  } under "${category}"`
+  console.log('Category on the page is ', categoryHeader)
   return (
     <SidebarLayout location={location}>
-      This is a category template
+      <div>
+        <h1>{categoryHeader}</h1>
+        <SeeAllCategoriesWrapper>
+          <Link to="/categories">See all categories</Link>
+        </SeeAllCategoriesWrapper>
+        <div>
+          {edges.map(({ node }) => (
+            <PostListItem node={node} />
+          ))}
+        </div>
+      </div>
     </SidebarLayout>
   )
 }
