9ddbd590d351f0f38055a4a107ef5b5e00c60c90
Hitarth Sharma <hitarth.sharma@outlook.com>
2018-12-02 13:44:15 +1100

    Removed unnecessary imports from category template

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import styled from 'styled-components'
+import { graphql } from 'gatsby'
 
 import SidebarLayout from '../components/sidebarLayout'
 import PostListItem from '../components/postListItem'
