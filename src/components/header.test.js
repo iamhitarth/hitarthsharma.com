3a37c54a14fdc486d5e96ebdf17a384fdb25ae8e
Hitarth Sharma <hitarth.sharma@outlook.com>
2020-03-25 10:47:02 +1300

    Upgraded packages

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -1,20 +1,24 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, cleanup } from '@testing-library/react'
 import Header from './header'
 
 describe('Header', () => {
   const siteTitle = 'Hitarth Sharma'
-  const { getByText, container } = render(<Header siteTitle={siteTitle} />)
+
+  afterEach(cleanup)
 
   it('matches snapshot', () => {
+    const { container } = render(<Header siteTitle={siteTitle} />)
     expect(container.firstChild).toMatchSnapshot()
   })
 
   it('has site title', () => {
+    const { getByText } = render(<Header siteTitle={siteTitle} />)
     expect(getByText(siteTitle)).not.toBeNull()
   })
 
   it('has nav links', () => {
+    const { getByText } = render(<Header siteTitle={siteTitle} />)
     expect(getByText('Home')).not.toBeNull()
     expect(getByText('About')).not.toBeNull()
   })
--- src/components/postListItem.test.js
+++ src/components/postListItem.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, cleanup } from '@testing-library/react'
 import PostListItem from './postListItem'
 
 describe('Post list item', () => {
@@ -16,27 +16,32 @@ describe('Post list item', () => {
     },
   }
 
-  const { getByTestId, container } = render(<PostListItem node={node} />)
+  afterEach(cleanup)
 
   it('matches snapshot', () => {
+    const { container } = render(<PostListItem node={node} />)
     expect(container.firstChild).toMatchSnapshot()
   })
 
   it('displays post title', () => {
+    const { getByTestId } = render(<PostListItem node={node} />)
     expect(getByTestId('postTitle')).toHaveTextContent(node.frontmatter.title)
   })
 
   it('displays post date', () => {
+    const { getByTestId } = render(<PostListItem node={node} />)
     expect(getByTestId('postDate')).toHaveTextContent(node.fields.postDate)
   })
 
   it('displays read time', () => {
+    const { getByTestId } = render(<PostListItem node={node} />)
     expect(getByTestId('timeToRead')).toHaveTextContent(
       `${node.timeToRead} mins.`
     )
   })
 
   it('displays post excerpt', () => {
+    const { getByTestId } = render(<PostListItem node={node} />)
     expect(getByTestId('postExcerpt')).toHaveTextContent(node.excerpt)
   })
 })
