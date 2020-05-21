956a418b98090f39882c9b7342031f661d136513
Hitarth Sharma <hitarth.sharma@outlook.com>
2020-05-21 16:16:52 +1200

    Added ability to reorder list of things

--- src/pages/tools/goal-prioritisation.js
+++ src/pages/tools/goal-prioritisation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../../components/layout'
 import styled from 'styled-components'
+import { ReactSortable } from 'react-sortablejs'
 
 import { PostTitle } from '../../templates/blogPost'
 import confusedEmoji from '../../assets/images/emoji-confused-face.png'
@@ -8,7 +9,6 @@ import smilingSweatEmoji from '../../assets/images/emoji-smiling-with-sweat.png'
 import sunglassesEmoji from '../../assets/images/emoji-sunglasses.png'
 
 /* TODOs
-Make the list items sortable
 Export as PDF/image - probably not going to do this
 */
 
@@ -99,6 +99,11 @@ class GoalPrioritisationTool extends React.Component {
       ? smilingSweatEmoji
       : confusedEmoji
 
+  updateItemsOrder = (items) =>
+    this.setState({ items }, () => {
+      this.saveStateToLocalStorage()
+    })
+
   componentDidMount = () => {
     this.itemTextInput.current.focus()
     const controlSpectrumDataString = localStorage.getItem(CONTROL_SPECTRUM_KEY)
@@ -109,7 +114,11 @@ class GoalPrioritisationTool extends React.Component {
 
   renderItems = (items) => {
     return (
-      <ul style={{ marginLeft: '0' }}>
+      <ReactSortable
+        list={items}
+        setList={this.updateItemsOrder}
+        style={{ marginLeft: '0' }}
+      >
         {items.map((item, i) => (
           <li
             key={item.id}
@@ -151,7 +160,7 @@ class GoalPrioritisationTool extends React.Component {
             />
           </li>
         ))}
-      </ul>
+      </ReactSortable>
     )
   }
 
